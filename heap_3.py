56de66411f3ca3d68678805380f695f6bbd1e4fc
Oleg <77359371+JohnPepperys@users.noreply.github.com>
2021-01-20 00:16:06 +0300

    work with linked file

---
+++ heap_3.py
@@ -0,0 +1,96 @@
+# ---------- file:      heap_3.py
+# ---------- project:   test_algoritms
+# ---------- author:    O.Trushman
+# ---------- data:      19/01/2021
+
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+def my_Check_Last_Element_in_Heap(heap, nowpos):
+    heapsize = len(heap)
+    i = nowpos
+    while i != 0:
+        if heap[i] > heap[(i - 1) // 2]:
+            heap[i], heap[(i - 1) // 2] = heap[(i - 1) // 2], heap[i]
+        else:
+            break
+        i = i // 2
+
+        # print(i, heap)
+    if heap[0] < heap[1]:
+        heap[0], heap[1] = heap[1], heap[0]
+    elif heapsize > 2 and heap[0] < heap[2]:
+        heap[0], heap[2] = heap[2], heap[0]
+
+
+
+def q_insert(heap, num):
+    # there are no elements in heap
+    if len(heap) == 0:
+        heap.append(num)
+
+    # we have one or more element in heap
+    else:
+        heap.append(num)
+        qq = len(heap)
+        my_Check_Last_Element_in_Heap(heap, qq - 1)
+
+
+
+
+def q_extract_max(heap):
+    maxval = heap[0]
+    heap[0] = -1
+    heap[0], heap[-1] = heap[-1], heap[0]
+    heap.pop(-1)
+    heapsize = len(heap)
+    i = 0
+    while i < heapsize:
+        # print(i, heapsize, heap)
+        if heapsize > (2 * i + 2):          
+            if heap[2 * i + 1] > heap[2 * i + 2]:
+                if heap[i] < heap[2 * i + 1]:
+                    heap[i], heap[2 * i + 1] = heap[2 * i + 1], heap[i]
+                    i = i * 2 + 1
+                else:
+                    break
+            else:
+                if heap[i] < heap[2 * i + 2]:
+                    heap[i], heap[2 * i + 2] = heap[2 * i + 2], heap[i]
+                    i = i * 2 + 2
+                else:
+                    break
+
+        elif heapsize > (2 * i + 1):
+            if heap[i] < heap[2 * i + 1]:
+                heap[i], heap[2 * i + 1] = heap[2 * i + 1], heap[i]
+            i = i * 2 + 1
+            break
+        else:
+            break
+
+    # print(maxval, heap)
+    return maxval
+
+# ---------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------
+
+
+# ----------------- MAIN ---------------------------------------------------------
+# -------------------------------- MAIN ------------------------------------------
+#heap = []
+#extract = []
+
+#n = int(input())
+#for i in range(n):
+#    s = input()
+#    if 'Insert ' in s:
+#        tmp = s.split(' ')
+#        q_insert(int(tmp[1]))
+
+#    if 'ExtractMax' in s:
+#        extract.append(q_extract_max())
+
+#for i in range(len(extract)):
+#    print(extract[i])
---
+++ heap_test_2.py
@@ -0,0 +1,51 @@
+# ---------- file:      heap_test_2.py
+# ---------- project:   test_algoritms
+# ---------- author:    O.Trushman
+# ---------- data:      19/01/2021
+
+# ---------------------------------------------------------------------------------------------------------
+
+import random
+from heap_3 import q_insert, q_extract_max
+
+def t_insert(x):
+    T.append(x)
+
+def t_extract_max():
+    m = max(T)
+    T.remove(m)
+    return m
+
+
+# ---------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------
+
+for j in range(500):
+    n = 10
+    T = []
+    Bigheap = []
+    stop = False
+
+    for i in range(n):
+        k = random.randint(0, 100)
+        q_insert(Bigheap, k)
+        t_insert(k)
+
+    print(j, 'T: ', T)
+    print(j, 'Heap: ', Bigheap)
+
+    for i in range(n):
+        a = q_extract_max(Bigheap)
+        b = t_extract_max()
+        if a == b:
+            print(i, a, Bigheap)
+        else:
+            print(i, a, b, Bigheap, "ERROR")
+            stop = True
+            break
+    
+    if stop:
+        break
+
